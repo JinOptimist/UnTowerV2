a2dec87d6301bc02b8cf49fed0e82f0dd6a52b6e
Pavel Lvou <Pavel.Lvou@thomsonreuters.com>
2019-11-26 14:26:13 +0300

    Init

---
+++ Assets/Script/BulletDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Script;
+using UnityEngine;
+
+public class ShotMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bullet colide");
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody Rigidbody;
+
+    public float EnemyVelocity = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var xVelocity = Random.value * EnemyVelocity * 2 - EnemyVelocity;
+        var zVelocity = Random.value * EnemyVelocity * 2 - EnemyVelocity;
+        Rigidbody.velocity = new Vector3(xVelocity, 0, zVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rigidbody.AddForce(0, 0, EnemyVelocity * Time.deltaTime, ForceMode.Acceleration);
+    }
+}
