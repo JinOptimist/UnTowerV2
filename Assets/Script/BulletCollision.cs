60258efd74e58daf1924e318dba72c2928d3b34e
88jin@mail.ru <88jin@mail.ru>
2019-11-26 16:15:25 +0300

    Add files

---
+++ Assets/Script/BulletCollision.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Script;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bullet colide");
+        if (collision.gameObject.CompareTag(ConstStore.EnemyTag))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag(ConstStore.GroundTag))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
