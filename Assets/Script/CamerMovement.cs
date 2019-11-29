1e23101c441b6ba75b0a341447dabed09345e3e6
Pavel Lvou <Pavel.Lvou@thomsonreuters.com>
2019-11-29 15:36:06 +0300

    Get coin

--- Assets/Script/CamerMovement.cs
+++ Assets/Script/CamerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CamerMovement : MonoBehaviour
 {
+    public bool FreeCameraMovement = false;
+
     private float moveSpeed = 0.5f;
     private float scrollSpeed = 10f;
 
@@ -21,19 +23,23 @@ public class CamerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0 || verticalInput != 0)
+        if (FreeCameraMovement)
         {
-            transform.position += moveSpeed * new Vector3(horizontalInput, 0, verticalInput);
-        }
-
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
-        {
-            //Check that we stil upper than ground
-            if (transform.position.y + -Input.GetAxis("Mouse ScrollWheel") * scrollSpeed > 0)
+            if (Input.GetAxisRaw("Horizontal") != 0 || verticalInput != 0)
             {
-                transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+                transform.position += moveSpeed * new Vector3(horizontalInput, 0, verticalInput);
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                //Check that we stil upper than ground
+                if (transform.position.y + -Input.GetAxis("Mouse ScrollWheel") * scrollSpeed > 0)
+                {
+                    transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+                }
             }
         }
+        
 
         //Debug.Log($"Camera position = {transform.position}");
     }
